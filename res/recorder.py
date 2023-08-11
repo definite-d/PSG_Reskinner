3b496c34dc2018bfe2ad1b613bdfcc7bec662499
definite-d <standing_straight@yahoo.co.uk>
2023-08-11 14:17:28 +0100

    Started on the recorder script.

---
+++ res/recorder.py
@@ -0,0 +1,5 @@
+"""
+This script is used to run the demo and automatically record a GIF of the demo window,
+then save the GIF. It relies on ShareX, and the shortcut for GIF recording being set.
+"""
+
