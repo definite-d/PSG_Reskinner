b327c8c76ee7bdc38844cbb56da70192d2fc3577
definite-d <38317208+definite-d@users.noreply.github.com>
2023-10-31 03:39:04 +0100

    Cleaning up and compiling...

--- psg_reskinner/exception.py
+++ psg_reskinner/exception.py
@@ -41,6 +41,38 @@
 #  furnished to do so, subject to the following conditions:
 #
 #
+#  PSG_Reskinner
+#
+#  Enables changing the themes of your PySimpleGUI windows and elements
+#  instantaneously on the fly without the need for re-instantiating the window.
+#
+#  MIT License
+#
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#
+#  PSG_Reskinner
+#
+#  Enables changing the themes of your PySimpleGUI windows and elements
+#  instantaneously on the fly without the need for re-instantiating the window.
+#
+#  MIT License
+#
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#
 class ReskinnerException(Exception):
     def __init__(self, message):
         """
