838d7c3055a874cfedf96905857cc859df025b25
definite-d <38317208+definite-d@users.noreply.github.com>
2023-10-31 03:49:36 +0100

    Still cleaning up.

--- psg_reskinner/__init__.py
+++ psg_reskinner/__init__.py
@@ -25,22 +25,6 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 
-#  PSG_Reskinner
-#
-#  Enables changing the themes of your PySimpleGUI windows and elements
-#  instantaneously on the fly without the need for re-instantiating the window.
-#
-#  MIT License
-#
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#
 name = "psg_reskinner"
 from .constants import HSL_INTERPOLATION, HUE_INTERPOLATION, RGB_INTERPOLATION
 from .psg_reskinner import animated_reskin, reskin, toggle_transparency
