da2a425b36d80fbc086c438a5488937cf954d49f
definite-d <38317208+definite-d@users.noreply.github.com>
2023-10-31 03:45:26 +0100

    Cleaning up and compiling...

--- psg_reskinner/exception.py
+++ psg_reskinner/exception.py
@@ -25,6 +25,7 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 
+
 class ReskinnerException(Exception):
     def __init__(self, message):
         """
